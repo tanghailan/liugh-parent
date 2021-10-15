a3ceaf86974b10d9cc464ec13b9863e06c9ae44b
tanghailan <tanghailan@88.com>
2021-10-15 11:11:01 +0800

    format code

--- liugh-web/src/main/java/com/liugh/aspect/AccessLimitAspect.java
+++ liugh-web/src/main/java/com/liugh/aspect/AccessLimitAspect.java
@@ -4,17 +4,15 @@ import com.google.common.collect.Maps;
 import com.google.common.util.concurrent.RateLimiter;
 import com.liugh.annotation.AccessLimit;
 import com.liugh.base.BusinessException;
-import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
-import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 /**
  * 限流切面
@@ -42,17 +40,19 @@ public class AccessLimitAspect extends AbstractAspectManager{
     @Override
     public Object execute(ProceedingJoinPoint pjp,Method method) throws Throwable{
         AccessLimit lxRateLimit = method.getAnnotation(AccessLimit.class);
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
+            .getRequestAttributes()).getRequest();
         // 或者url(存在map集合的key)
         String url = request.getRequestURI();
         if (!limitMap.containsKey(url)) {
             // 创建令牌桶
             rateLimiter = RateLimiter.create(lxRateLimit.perSecond());
             limitMap.put(url, rateLimiter);
-            log.info("<<=================  请求{},创建令牌桶,容量{} 成功!!!",url,lxRateLimit.perSecond());
+            log.info("<<=================  请求{},创建令牌桶,容量{} 成功!!!", url, lxRateLimit.perSecond());
         }
         rateLimiter = limitMap.get(url);
-        if (!rateLimiter.tryAcquire(lxRateLimit.timeOut(), lxRateLimit.timeOutUnit())) {//获取令牌
+        //获取令牌
+        if (!rateLimiter.tryAcquire(lxRateLimit.timeOut(), lxRateLimit.timeOutUnit())) {
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
             log.info("Error ---时间:{},获取令牌失败.", sdf.format(new Date()));
             throw new BusinessException("服务器繁忙，请稍后再试!");
